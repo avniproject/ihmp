83f3e11e24d8dda42c4918c963cf13a7ba11cd5f
Arjun <arjunk@thoughtworks.com>
2018-12-06 13:35:54 +0530

    monthly assessment needs advice generation

--- eligibleCouple/rules/monthlyNeedsAssessmentHandler.js
+++ eligibleCouple/rules/monthlyNeedsAssessmentHandler.js
@@ -6,7 +6,8 @@ import {
     FormElementStatusBuilder,
     StatusBuilderAnnotationFactory,
     FormElementStatus,
-    VisitScheduleBuilder
+    VisitScheduleBuilder,
+    complicationsBuilder as ComplicationsBuilder
 } from 'rules-config/rules';
 
 const MonthlyAssessmentViewFilter = RuleFactory("8b5bf56e-346a-486e-b00e-9fa604fa0b54", "ViewFilter");
@@ -47,6 +48,11 @@ class ECMonthlyNeedsAssessmentViewFilterHandlerIHMP {
     }
 
     @WithStatusBuilder
+    missedPeriodsAndNotDoneUptCounselling([], statusBuilder) {
+        statusBuilder.show().when.valueInEncounter("Whether undergone pregnancy test if missed monthly periods").is.no;
+    }
+
+    @WithStatusBuilder
     whetherUsingAnyFamilyPlanningMethodCurrently([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Whether currently pregnant").is.no
             .and.when.valueInEnrolment("Whether sterilized").is.no;
@@ -74,6 +80,48 @@ class ECMonthlyNeedsAssessmentViewFilterHandlerIHMP {
     }
 
     @WithStatusBuilder
+    ihmpFpCounsellingToDelayFirstConception([], statusBuilder) {
+        statusBuilder.show().when.ageInYears.is.lessThanOrEqualTo(19)
+            .and.valueInEnrolment("gravida").is.equals(0)
+            .and.valueInEnrolment("Whether sterilized").is.no;
+    }
+
+    @WithStatusBuilder
+    ihmpFpCounsellingForSpacingToNonUser([], statusBuilder) {
+        statusBuilder.show().when.valueInEnrolment("Age of youngest child").asAge.is.lessThan(2)
+            .and.when.valueInEncounter("Whether using any family planning method currently").is.no
+            .and.valueInEnrolment("Whether sterilized").is.no;
+    }
+
+    @WithStatusBuilder
+    ihmpFpCounsellingForSpacingToUser([], statusBuilder) {
+        statusBuilder.show().when.valueInEnrolment("Age of youngest child").asAge.is.lessThan(2)
+            .and.when.valueInEncounter("Whether using any family planning method currently").is.yes
+            .and.valueInEnrolment("Whether sterilized").is.no;
+
+    }
+
+    @WithStatusBuilder
+    ihmpFpCounsellingForSpacingToDesireToUseEc([], statusBuilder) {
+        statusBuilder.show().when.valueInEnrolment("Age of youngest child").asAge.is.lessThan(2)
+            .and.when.valueInEncounter("Whether desire to use any FP methods in future").is.yes
+            .and.valueInEnrolment("Whether sterilized").is.no;
+    }
+
+    @WithStatusBuilder
+    ihmpFpCounsellingInformationRegardingSterilization([], statusBuilder) {
+        statusBuilder.show().when.valueInEnrolment("Number of living children").is.greaterThanOrEqualTo(2)
+            .and.valueInEnrolment("Whether sterilized").is.no;
+    }
+
+    @WithStatusBuilder
+    ihmpIpilCounselling([], statusBuilder) {
+        statusBuilder.show().when.valueInEnrolment("Whether sterilized").is.no
+    }
+
+
+    
+    @WithStatusBuilder
     durationOfSymptomsInDays([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Symptoms of RTI").is.defined
             .and.not.when.valueInEncounter("Symptoms of RTI").containsAnswerConceptName("None");
@@ -81,6 +129,12 @@ class ECMonthlyNeedsAssessmentViewFilterHandlerIHMP {
 
     @WithStatusBuilder
     whetherTreatmentForRtiSymptomSTaken([], statusBuilder) {
+        statusBuilder.show().when.valueInEncounter("Symptoms of RTI").is.defined
+            .and.not.when.valueInEncounter("Symptoms of RTI").containsAnswerConceptName("None");
+    }
+
+    @WithStatusBuilder
+    ihmpRecommendationForRtiSymptoms([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Symptoms of RTI").is.defined
             .and.not.when.valueInEncounter("Symptoms of RTI").containsAnswerConceptName("None");
     }
@@ -96,5 +150,28 @@ class ECMonthlyNeedsAssessmentViewFilterHandlerIHMP {
     }
 }
 
+const MonthlyNeedsAssessmentDecision = RuleFactory("8b5bf56e-346a-486e-b00e-9fa604fa0b54", "Decision");
 
-module.exports = {ECMonthlyNeedsAssessmentViewFilterHandlerIHMP};
+@MonthlyNeedsAssessmentDecision("5abefe5a-eba8-4779-b403-0f62358cafb4", "IHMP EC Monthly needs assessment decisions", 100.0, {})
+class ECMonthlyNeedsAssessmentDecisionIHMP {
+    static exec(programEncounter, decisions, context, today) {
+        const complicationsBuilder = new ComplicationsBuilder({
+            programEncounter: programEncounter,
+            complicationsConcept: 'Program enrolment advice'
+        });
+        complicationsBuilder.addComplication("Enrol in Mother program").when.valueInEncounter("Whether currently pregnant").is.yes
+            .and.not.valueInLastEncounter("Whether currently pregnant").is.yes
+            .and.whenItem(_.some(programEncounter.programEnrolment.individual.enrolments, ['program.name', 'Mother'])).is.falsy;
+
+        decisions.encounterDecisions.push(complicationsBuilder.getComplications());
+        return decisions;
+    }
+
+
+
+
+}
+
+
+
+module.exports = {ECMonthlyNeedsAssessmentViewFilterHandlerIHMP, ECMonthlyNeedsAssessmentDecisionIHMP};
