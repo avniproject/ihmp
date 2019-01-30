e0bea0e3d7a6fa26f4c36c04e137e30c428166ad
Arjun <arjunk@thoughtworks.com>
2019-01-30 11:49:50 +0530

    1172 | ANC VHND Follow up date

--- pregnancy/rules/ancVhndHandler.js
+++ pregnancy/rules/ancVhndHandler.js
@@ -10,10 +10,10 @@ import {
     RuleCondition
 } from 'rules-config/rules';
 
-const ANCASHAViewFilter = RuleFactory("e2cf41de-d962-4110-9d66-70877e2eb59d", "ViewFilter");
+const ANCVHNDViewFilter = RuleFactory("e2cf41de-d962-4110-9d66-70877e2eb59d", "ViewFilter");
 const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
 
-@ANCASHAViewFilter("18ae0315-e6c7-4d8c-89fa-527e2f8712ca", "IHMP ANC VHND View Filter", 100.0, {})
+@ANCVHNDViewFilter("18ae0315-e6c7-4d8c-89fa-527e2f8712ca", "IHMP ANC VHND View Filter", 100.0, {})
 class PregnancyANCVHNDViewFilterHandlerIHMP {
     static exec(programEncounter, formElementGroup, today) {
         return FormElementsStatusHelper
@@ -37,4 +37,19 @@ class PregnancyANCVHNDViewFilterHandlerIHMP {
 
 }
 
-module.exports = {PregnancyANCVHNDViewFilterHandlerIHMP};
+const ANCASHAFollowupViewFilter = RuleFactory("e2cf41de-d962-4110-9d66-70877e2eb59d", "ViewFilter");
+
+@ANCASHAFollowupViewFilter("18ae0315-e6c7-4d8c-89fa-527e2f8712ca", "IHMP ANC VHND View Filter", 100.0, {})
+class PregnancyANCVHNDFollowupViewFilterHandlerIHMP {
+    static exec(programEncounter, formElementGroup, today) {
+        return FormElementsStatusHelper
+            .getFormElementsStatusesWithoutDefaults(new PregnancyANCVHNDViewFilterHandlerIHMP(), programEncounter, formElementGroup, today);
+    }
+    
+    @WithStatusBuilder
+    placeOfAncVhndReferralUtilized([], statusBuilder) {
+        statusBuilder.show().when.valueInEncounter("Whether ANC VHND referral utilized").is.yes;
+    }
+}
+
+module.exports = {PregnancyANCVHNDViewFilterHandlerIHMP,PregnancyANCVHNDFollowupViewFilterHandlerIHMP};
