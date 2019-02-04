e211deb05f3c12bdf155b30629b28a50cafa4db7
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-02-04 15:25:03 +0530

    1074 | Hide 3rd Trimester Preg-Complications and show TT1Date only once, in Pregnancy/AshaVHND

--- pregnancy/rules/ancVhndHandler.js
+++ pregnancy/rules/ancVhndHandler.js
@@ -1,20 +1,18 @@
 import lib from "../../lib";
-
-const moment = require("moment");
-const _ = require("lodash");
 import {
-    RuleFactory,
     FormElementsStatusHelper,
-    FormElementStatusBuilder,
-    StatusBuilderAnnotationFactory,
     FormElementStatus,
-    VisitScheduleBuilder,
-    RuleCondition
+    FormElementStatusBuilder,
+    RuleCondition,
+    RuleFactory,
+    StatusBuilderAnnotationFactory,
+    VisitScheduleBuilder
 } from 'rules-config/rules';
 
 const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
 
 const ANCVHNDViewFilter = RuleFactory("e2cf41de-d962-4110-9d66-70877e2eb59d", "ViewFilter");
+
 @ANCVHNDViewFilter("18ae0315-e6c7-4d8c-89fa-527e2f8712ca", "IHMP ANC VHND View Filter", 100.0, {})
 class PregnancyANCVHNDViewFilterHandlerIHMP {
     static exec(programEncounter, formElementGroup, today) {
@@ -22,24 +20,36 @@ class PregnancyANCVHNDViewFilterHandlerIHMP {
             .getFormElementsStatusesWithoutDefaults(new PregnancyANCVHNDViewFilterHandlerIHMP(), programEncounter, formElementGroup, today);
     }
 
+
     @WithStatusBuilder
     tt1Date([], statusBuilder) {
-        statusBuilder.show().when.latestValueInPreviousEncounters("TT1 Date").is.notDefined;
-    }
-    
-    @WithStatusBuilder
-    tt2Date([], statusBuilder) {
-        statusBuilder.show().when.latestValueInPreviousEncounters("TT2 Date").is.notDefined;
-    }
-    
-    @WithStatusBuilder
-    ttBoosterDate([], statusBuilder) {
-        statusBuilder.show().when.latestValueInPreviousEncounters("TT Booster Date").is.notDefined;
+        statusBuilder.show().when.valueInEntireEnrolment('TT1 Date').is.notDefined
+            .or.when.valueInEncounter('TT1 Date').is.defined;
     }
 
+    @WithStatusBuilder
+    tt2Date([], statusBuilder) {
+        statusBuilder.show().when.valueInEntireEnrolment('TT2 Date').is.notDefined
+            .or.when.valueInEncounter('TT2 Date').is.defined;
+    }
+
+    @WithStatusBuilder
+    ttBoosterDate([], statusBuilder) {
+        statusBuilder.show().when.valueInEntireEnrolment('TT Booster Date').is.notDefined
+            .or.when.valueInEncounter('TT Booster Date').is.defined;
+    }
+
+    @WithStatusBuilder
+    pregnancyComplications([programEncounter, fe, today], statusBuilder) {
+        const currentTrimester = lib.calculations.currentTrimester(programEncounter.programEnrolment, today);
+        if (currentTrimester !== 3) {
+            statusBuilder.skipAnswers('Reduced fetal movement', 'Watery discharge before onset of labour');
+        }
+    }
 }
 
 const ANCASHAFollowupViewFilter = RuleFactory('5edbca3a-04bf-4e16-b624-57331a71da39', "ViewFilter");
+
 @ANCASHAFollowupViewFilter("243be415-dc0a-429e-afff-938c493d2de5", "IHMP ANC VHND Followup View Filter", 100.0, {})
 class PregnancyANCVHNDFollowupViewFilterHandlerIHMP {
     static exec(programEncounter, formElementGroup, today) {
@@ -61,4 +71,4 @@ class PregnancyANCVHNDFollowupViewFilterHandlerIHMP {
     }
 }
 
-module.exports = {PregnancyANCVHNDViewFilterHandlerIHMP,PregnancyANCVHNDFollowupViewFilterHandlerIHMP};
+module.exports = {PregnancyANCVHNDViewFilterHandlerIHMP, PregnancyANCVHNDFollowupViewFilterHandlerIHMP};
