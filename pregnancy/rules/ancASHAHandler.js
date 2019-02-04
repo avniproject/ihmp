d2148d0142b7892587f37d5c122a629fa07d4a33
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-02-04 14:49:36 +0530

    1073 | In Pregnancy/AshaAnc form, show TT1 only once, hide 3rd Trimester Pregnancy-Complications

--- pregnancy/rules/ancASHAHandler.js
+++ pregnancy/rules/ancASHAHandler.js
@@ -1,13 +1,13 @@
-const moment = require("moment");
-const _ = require("lodash");
+import moment from 'moment';
+import lib from '../../lib';
 import {
-    RuleFactory,
     FormElementsStatusHelper,
-    FormElementStatusBuilder,
-    StatusBuilderAnnotationFactory,
     FormElementStatus,
-    VisitScheduleBuilder,
-    RuleCondition
+    FormElementStatusBuilder,
+    RuleCondition,
+    RuleFactory,
+    StatusBuilderAnnotationFactory,
+    VisitScheduleBuilder
 } from 'rules-config/rules';
 
 const ANCASHAViewFilter = RuleFactory("4201c189-5a6c-40ca-8c33-c5b7d24a9604", "ViewFilter");
@@ -25,7 +25,7 @@ class PregnancyANCASHAViewFilterHandlerIHMP {
         statusBuilder.show().when.latestValueInPreviousEncounters("Whether registered for antenatal care").is.notDefined
             .or.latestValueInPreviousEncounters("Whether registered for antenatal care").is.no;
     }
-    
+
     @WithStatusBuilder
     dateOfRegistrationForAntenatalCare([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Whether registered for antenatal care").is.yes;
@@ -41,22 +41,22 @@ class PregnancyANCASHAViewFilterHandlerIHMP {
     placeWhereRegisteredForAntenatalCare([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Whether registered for antenatal care").is.yes;
     }
-    
+
     @WithStatusBuilder
     whetherTreatmentTakenForAntenatalComplications([], statusBuilder) {
         statusBuilder.show().when.not.valueInEncounter("Pregnancy complications").containsAnswerConceptName("No problem");
     }
-    
+
     @WithStatusBuilder
     whetherTheAntenatalComplicationsIsAddressed([], statusBuilder) {
         statusBuilder.show().when.not.valueInEncounter("Pregnancy complications").containsAnswerConceptName("No problem");
     }
-    
+
     @WithStatusBuilder
     placeWhereAntenatalExaminationDone([], statusBuilder) {
         statusBuilder.show().when.valueInEncounter("Whether antenatal examination done").is.yes;
     }
-    
+
     @WithStatusBuilder
     ihmpAshaCounsellingForPregnancyConfirmedAndNotRegisteredForAnc([], statusBuilder) {
         statusBuilder.show().when.latestValueInAllEncounters("Whether registered for antenatal care").is.no;
@@ -66,7 +66,7 @@ class PregnancyANCASHAViewFilterHandlerIHMP {
     ihmpAshaCounsellingForMonthlyAntenatalCheckup([], statusBuilder) {
         statusBuilder.show().when.latestValueInAllEncounters("Whether registered for antenatal care").is.yes;
     }
-    
+
     @WithStatusBuilder
     ihmpAshaCounsellingDuringEveryContactPost7thMonth([], statusBuilder) {
         statusBuilder.show().whenItem(moment(statusBuilder.context.programEncounter).diff(statusBuilder.context.programEncounter.programEnrolment.getObservationValue('Last menstrual period'), 'months', true)).is.greaterThan(7);
@@ -80,6 +80,20 @@ class PregnancyANCASHAViewFilterHandlerIHMP {
     @WithStatusBuilder
     ihmpAshaCounsellingDuringEveryContactPost8thMonth([], statusBuilder) {
         statusBuilder.show().whenItem(moment(statusBuilder.context.programEncounter).diff(statusBuilder.context.programEncounter.programEnrolment.getObservationValue('Last menstrual period'), 'months', true)).is.greaterThan(8);
+    }
+
+    @WithStatusBuilder
+    tt1Date([], statusBuilder) {
+        statusBuilder.show().when.valueInEntireEnrolment('TT1 Date').is.notDefined
+            .or.when.valueInEncounter('TT1 Date').is.defined;
+    }
+
+    @WithStatusBuilder
+    pregnancyComplications([programEncounter, fe, today], statusBuilder) {
+        const currentTrimester = lib.calculations.currentTrimester(programEncounter.programEnrolment, today);
+        if (currentTrimester !== 3) {
+            statusBuilder.skipAnswers('Reduced fetal movement', 'Watery discharge before onset of labour');
+        }
     }
 }
 
