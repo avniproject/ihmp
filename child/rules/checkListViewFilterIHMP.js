e9ee9f1977d22b54cc98848bf74699872af64f9c
Vinod Deolal <vinoddeolal@gmail.com>
2019-02-26 18:54:36 +0530

    child immunisation rule changes

--- child/rules/checkListViewFilterIHMP.js
+++ child/rules/checkListViewFilterIHMP.js
@@ -9,21 +9,19 @@ import {
 } from 'rules-config/rules';
 
 const ViewFilter = RuleFactory("161cecc6-9a8a-4bc8-b577-f1ea8c6c5702", "ViewFilter");
-const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
+const WithStatusBuilder = StatusBuilderAnnotationFactory('checklistItem', 'formElement');
 
 @ViewFilter("b730ce48-108a-4c16-8838-4a575e467a6f", "IHMP Check List View Filter", 100.0, {})
 class CheckListViewFilterIHMP {
-    static exec(programEncounter, formElementGroup, today) {
-        console.log("came to CheckListViewFilterIHMP");
+    static exec(checklistItem, formElementGroup, today) {
         return FormElementsStatusHelper
-            .getFormElementsStatusesWithoutDefaults(new CheckListViewFilterIHMP(), programEncounter, formElementGroup, today);
+            .getFormElementsStatusesWithoutDefaults(new CheckListViewFilterIHMP(), checklistItem, formElementGroup, today);
     }
 
     @WithName("Administered By(Other)")
     @WithStatusBuilder
-    other(programEncounter, statusBuilder) {
-        console.log(statusBuilder.context.programEncounter);
-        statusBuilder.show().when.valueInEncounter("Administered by").containsAnswerConceptName("Other");
+    other([checklistItem], statusBuilder) {
+        statusBuilder.show().when.valueInChecklistItem("Administered by").containsAnswerConceptName("Other");
     }
 
 }
