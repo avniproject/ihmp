57e9b0ba105f70a2ac6553a6839e80d33cf76b93
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-05-02 13:20:08 +0530

    0000 | Create organisation only when not present

--- create_organisation.sql
+++ create_organisation.sql
@@ -1,10 +1,6 @@
-CREATE ROLE ihmp NOINHERIT PASSWORD 'password';
-
-GRANT ihmp TO openchs;
-
-GRANT ALL ON ALL TABLES IN SCHEMA public TO ihmp;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ihmp;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO ihmp;
+select create_db_user('ihmp', 'password');
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-    SELECT 'IHMP', 'ihmp', '11845014-41e6-42fa-bbc8-57b1bfcd6f57', id FROM organisation WHERE name = 'OpenCHS';
+SELECT 'IHMP', 'ihmp', '11845014-41e6-42fa-bbc8-57b1bfcd6f57', id
+FROM organisation
+WHERE name = 'OpenCHS' and not exists (select 1 from organisation where name = 'IHMP');
