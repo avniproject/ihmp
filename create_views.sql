f7ae4cb8e4c3aafb909407ad0ccfa6f298a91dd0
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-05-07 08:48:23 +0530

    0000 | Remove unnecessary permission setup on implementation specific views
    Noted by the implementation
     and by default are not accessible by other implementations

--- create_views.sql
+++ create_views.sql
@@ -1295,11 +1295,3 @@ create view ihmp_abortion_view as (
 );
 -- ----------------------------------------------------
 set role none;
-
-SELECT grant_all_on_all(a.rolname)
-FROM pg_roles a
-WHERE pg_has_role('openchs', a.oid, 'member')
-  and a.rolsuper is false
-  and a.rolname not like 'pg%'
-  and a.rolname not like 'rds%'
-order by a.rolname;
