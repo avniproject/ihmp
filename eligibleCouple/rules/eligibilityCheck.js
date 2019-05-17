f7d076ad6a7e71ce4e896c29bef045f3a2e8a116
Vinay Venu <ijkvinay@gmail.com>
2019-05-17 15:47:16 +0530

    1381 | Add eligibility check for Eligible couple program

---
+++ eligibleCouple/rules/eligibilityCheck.js
@@ -0,0 +1,20 @@
+import {EnrolmentEligibilityCheck} from 'rules-config/rules';
+
+
+@EnrolmentEligibilityCheck({
+    name: 'EligibleCoupleEnrolmentEligibility',
+    uuid: '2294aafa-8fe1-488b-8d97-a5270c676bac',
+    programUUID: 'fdf5c253-c49f-43e1-9591-4556a3ea36d4',
+    executionOrder: 100.0,
+    metadata: {}
+})
+class EligibleCoupleEnrolmentEligibility {
+    static exec({individual}) {
+        console.log('yeahhhhhh');
+        return individual.isFemale() && individual.getAgeInYears() > 5;
+    }
+}
+
+export  {
+    EligibleCoupleEnrolmentEligibility
+};
