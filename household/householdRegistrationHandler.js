b1046fd3d07629173201b712fc099a35cca05f18
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-10-21 15:52:10 +0530

    add uuid for household form reg in rules

--- household/householdRegistrationHandler.js
+++ household/householdRegistrationHandler.js
@@ -10,7 +10,8 @@ import {
     WithName,
 } from 'rules-config/rules';
 
-const RegistrationViewFilter = RuleFactory("b9762a13-fe53-4a33-a76c-21953b30f38f", "ViewFilter");
+const RegistrationViewFilter = RuleFactory("b9762a13-fe53-4a33-a76c-21953b30f38f", "ViewFilter");//note for UAT only
+//const RegistrationViewFilter = RuleFactory("471243cf-2df9-4089-89a8-fe904e3ca68f", "ViewFilter") //todo the registration form uuid for household is different for prod and uat
 const WithStatusBuilder = StatusBuilderAnnotationFactory('individual', 'formElement');
 
 
